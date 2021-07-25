31ccf12c76ff14d02562c8c4fbc62a7745f08937
Marcos Amorim <markstm10@gmail.com>
2021-07-25 00:20:27 -0300

    🛠 Updated Sidebar para funcionar separado do App.tsx. Ainda sem a logica

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "../components/Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface SidebarProps {
+  genres: Array<GenreResponseProps>;
+  clickButtonAction: (id: number) => void;
+  selectedGenre: GenreResponseProps;
+}
+
+export function SideBar(props: SidebarProps) {
+  console.log(props);
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.clickButtonAction(genre.id)}
+            selected={props.selectedGenre.id === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
